32ae6133350e7a9f1fcba897ce07bd36b8ac8921
Freewind <nowindlee@gmail.com>
2023-02-24 01:02:18 +0800

    typescript-axios-web-access-redirect-302-demo

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-TypeScript Vite HTML Demo
+TypeScript Axios Web Access Redirect 302 Demo
 ===========================
+
+在浏览器中，axios也无法拿到302返回的location。`maxRedirects:0`无效，会自动跳转。
 
 ```
 npm install
-npm run demo
+npm start
 ```
 
 It will open page on browser automatically.
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,12 @@
-import './index.css';
+import axios from 'axios';
 
-document.getElementById("main")!.innerText = "Hello!"
+const button = document.querySelector<HTMLButtonElement>('#button')!;
+
+button.addEventListener('click', async () => {
+    const response = await axios.get('http://httpbin.org/redirect-to?url=get', {
+        maxRedirects: 0
+    })
+    console.log("### response", response)
+    const html = response.data;
+    console.log(html);
+})
